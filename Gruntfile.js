fadf5b80c7304d34d7e108d4af7edaae52b60e04
Claudiu <claudiu.cmc@gmail.com>
2016-01-12 17:34:02 +0100

    server und watch in einem

--- Gruntfile.js
+++ Gruntfile.js
@@ -114,5 +114,5 @@ module.exports = function(grunt) {
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'nodeunit', 'concat', 'uglify']);
-  grunt.registerTask('server',['connect']);
+  grunt.registerTask('server',['connect', 'watch']);
 };
