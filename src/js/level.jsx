a81a40ad2a4192ac34a3312172dad48393941f21
Claudiu <claudiu.cmc@gmail.com>
2016-05-29 16:14:48 +0200

    level rendered

--- src/js/level.jsx
+++ src/js/level.jsx
@@ -1,19 +1,56 @@
-var Level = React.createClass({
+/*
+beinhaltet dargestellten SpielZeilen
+*/
+var Level= React.createClass({
 	render: function() {
+
+		var spielzeilen= this.props.data.map(function(zeile) {
+			return (<SpielZeile data= {zeile} key= {zeile.id} />);
+		});
+
+
 		return (
 			<div className="level" class="level" x="x">
-				<Spielstein />
+				{spielzeilen}
 			</div>
 
 			);
 	}
 });
 
-var Spielstein = React.createClass({
+/*
+beinhaltet alle Spielsteine in einer Zeile, Lücken sind KeineSpielSteine
+*/
+var SpielZeile= React.createClass({
 	render: function() {
-		return (<div className="spielstein" class="spielstein">x</div>);
+		//var spielzeile= this.props.data.map(function(zeile) {
+			//return (zeile.steine.map(function(zeichen)){
+		var spalte= 0;
+		var spielsteine= this.props.data.steine.map(function(stein){
+			spalte++;
+			if(stein== 'x'){
+				return (<SpielStein data={stein} key={spalte} />);
+			}else {
+				return (<KeinSpielStein key={spalte}/>);
+			}
+			
+		});
+		return (<div className="SpielZeile" class="SpielZeile">{spielsteine}</div>);
 	}
 });
 
-ReactDOM.render(<Level />, document.getElementById('level'));
-ReactDOM.render(<Level />, document.getElementById('level'));
+var SpielStein= React.createClass({
+	render: function() {
+		return (<span className= "SpielStein" ></span>);
+	}
+});
+
+var KeinSpielStein= React.createClass({
+	render: function(){
+		return (<span className= "KeinSpielStein" />);
+	}
+});
+var level1= [
+	{id: 1, steine:['x', 'x', 'x', 'x', ' ']}];
+
+ReactDOM.render(<Level data={level1}/>, document.getElementById('level'));
