69083dce674b33a137b531a6b8963118bcb42262
Claudiu <claudiu.cmc@gmail.com>
2016-06-27 21:44:42 +0200

    weniger code

--- src/presentational/KeinSpielStein.jsx
+++
@@ -1,9 +0,0 @@
-import React, {createClass} from 'react';
-
-const KeinSpielStein= createClass({
-	render: function(){
-		return (<div className= "KeinSpielStein" />);
-	}
-});
-
-export {KeinSpielStein as default};
